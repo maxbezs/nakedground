078dbaeb5b9cdf4f73b2f385c8151d02e62030c6
maxbezs <baks.bezsmertnuy@gmail.com>
2024-03-27 02:36:56 +0100

    1

--- src/components/House.js
+++ src/components/House.js
@@ -1,16 +1,13 @@
 import { useState, useContext } from "react";
 import LangContext from "../contexts/LangContext";
-
+import ngVideo from "../images/NG-video.mp4";
 import languageMap from "../languageMap";
 const House = () => {
   const { globalVariable, updateGlobalVariable } = useContext(LangContext);
   return (
     <div className="house-section video-container">
       <video autoPlay muted loop playsInline>
-        <source
-          src="https://github.com/maxbezs/nakedground-front/releases/download/publish/NG-video.mp4"
-          type="video/mp4"
-        />
+        <source src={ngVideo} type="video/mp4" />
       </video>
       <div className="caption">
         <h2 className="hh2" style={{ color: "white" }}>
--- src/pages/CoffeePage.js
+++ src/pages/CoffeePage.js
@@ -1,62 +1,52 @@
-import Footer from '../components/Footer'
-import Form from '../components/Form'
-import Header from '../components/Header'
-import theBest from '../images/the-best.png'
-import '../styles/CoffeePage.css';
-import beans from '../images/bean-back.jpg'
-import TapeStrip from '../images/TapeStrip.png'
-import LangContext from '../contexts/LangContext';
-import languageMap from '../languageMap';
-import { useState,useContext, useEffect } from "react";
-const CoffeePage = () =>{
-    const { globalVariable, updateGlobalVariable } = useContext(LangContext);
+import Footer from "../components/Footer";
+import Form from "../components/Form";
+import Header from "../components/Header";
+import theBest from "../images/the-best.png";
+import "../styles/CoffeePage.css";
+import beans from "../images/bean-back.jpg";
+import TapeStrip from "../images/TapeStrip.png";
+import LangContext from "../contexts/LangContext";
+import languageMap from "../languageMap";
+import coffeeVideo from "../images/COFFEE-video.mp4";
 
-    useEffect(() => {
-        console.log("Global Lang: "+ globalVariable);
-      }, []);
-      return(
-        <div>
-            <Header />
-                <div className='coffeee-section'>
-                    <h1 className="hh2" >
-                        {languageMap[globalVariable]?.text19}
-                    </h1>
-                    <p className="hh2-span">{languageMap[globalVariable]?.text20}</p>
-                    <div className='bean-p-div'>
-                        <p className='bean-p'>
-                            {languageMap[globalVariable]?.text50}
-                        </p>
-                        <hr className='hr-coffee'/>
+import { useState, useContext, useEffect } from "react";
+const CoffeePage = () => {
+  const { globalVariable, updateGlobalVariable } = useContext(LangContext);
 
-                        <p className='bean-p'>
-                            {languageMap[globalVariable]?.text50a}
-                        </p>
-                    </div>
-                    
-                </div>
-                <div className='coffee-background'>
-                    <video autoPlay muted loop playsInline>
-                        <source src='https://github.com/maxbezs/nakedground-front/releases/download/publish/COFFEE-video.mp4' type="video/mp4" />
-                    </video>
-                    <div className="caption">
-                        <img className='tape-strip phh' src={TapeStrip}></img>
-                        
-                        <img src={theBest} className='the-best-image'></img>
-                    </div>
-                    
-                </div>
-                
-                <div className='coffee-story'>
-                    <p className='bean-p'>
-                            {languageMap[globalVariable]?.text51}
-                    </p>
-                    <hr className='hr-coffee'/>
-                    <p className='bean-p'>
-                            {languageMap[globalVariable]?.text51a}
-                    </p>
-                </div>
-            <Footer/>
+  useEffect(() => {
+    console.log("Global Lang: " + globalVariable);
+  }, []);
+  return (
+    <div>
+      <Header />
+      <div className="coffeee-section">
+        <h1 className="hh2">{languageMap[globalVariable]?.text19}</h1>
+        <p className="hh2-span">{languageMap[globalVariable]?.text20}</p>
+        <div className="bean-p-div">
+          <p className="bean-p">{languageMap[globalVariable]?.text50}</p>
+          <hr className="hr-coffee" />
+
+          <p className="bean-p">{languageMap[globalVariable]?.text50a}</p>
         </div>
-    )
-}
-export default CoffeePage
+      </div>
+      <div className="coffee-background">
+        <video autoPlay muted loop playsInline>
+          <source src={coffeeVideo} type="video/mp4" />
+        </video>
+        <div className="caption">
+          <img className="tape-strip phh" src={TapeStrip}></img>
+
+          <img src={theBest} className="the-best-image"></img>
+        </div>
+      </div>
+
+      <div className="coffee-story">
+        <p className="bean-p">{languageMap[globalVariable]?.text51}</p>
+        <hr className="hr-coffee" />
+        <p className="bean-p">{languageMap[globalVariable]?.text51a}</p>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default CoffeePage;
